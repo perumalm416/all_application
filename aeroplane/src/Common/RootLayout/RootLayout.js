09330a738f42fc732eb161412bc7b4b0864e48fc
perumalm416 <perumalm416@gmail.com>
2023-01-22 11:30:34 +0530

    main-navigation-style

---
+++ aeroplane/src/Common/RootLayout/RootLayout.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import { MainNavigation } from "./MainNavigation";
+
+export const RootLayout = () => {
+  return (
+    <Fragment>
+      <MainNavigation />
+      <h2>RootLayout page</h2>
+    
+    </Fragment>
+  );
+};
